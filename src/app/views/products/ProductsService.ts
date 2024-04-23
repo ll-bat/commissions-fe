f84bee8325c4a5e78b1faa8d7dee7214a3797004
vladimer <gshatakishvili@gmail.com>
2024-04-23 20:48:35 +0400

    Fetch products and categories from backend

--- src/app/views/products/ProductsService.ts
+++ src/app/views/products/ProductsService.ts
@@ -1,60 +1,14 @@
 import { type Result } from "@/app/types/requestTypes";
 import { type Product, type ProductCategory } from "@/app/views/products/types";
-
-const STATIC_CATEGORIES: ProductCategory[] = [
-  {
-    id: "#1",
-    name: "Category 1",
-  },
-  {
-    id: "#2",
-    name: "Category 2",
-  },
-  {
-    id: "#3",
-    name: "Category 3",
-  },
-];
-
-const STATIC_PRODUCTS_DATA: Product[] = [
-  {
-    id: "#1",
-    name: "Product 1",
-    category: STATIC_CATEGORIES[0],
-    price: 100,
-    commissionPercent: null,
-  },
-  {
-    id: "#2",
-    name: "Product 2",
-    category: STATIC_CATEGORIES[1],
-    price: 200,
-    commissionPercent: null,
-  },
-  {
-    id: "#3",
-    name: "Product 3",
-    category: STATIC_CATEGORIES[2],
-    price: 300,
-    commissionPercent: null,
-  },
-];
+import axiosClient from "@/app/axios";
 
 const productsService = {
   async getProducts(): Promise<Result<Product[]>> {
-    return Promise.resolve({
-      ok: true,
-      result: STATIC_PRODUCTS_DATA,
-      errors: null,
-    });
+    return await axiosClient.get("/products");
   },
 
   async getCategories(): Promise<Result<ProductCategory[]>> {
-    return Promise.resolve({
-      ok: true,
-      result: STATIC_CATEGORIES,
-      errors: null,
-    });
+    return await axiosClient.get("/categories");
   },
 };
 
